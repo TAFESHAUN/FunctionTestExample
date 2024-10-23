1aa515ac590c6d539080986547ae313aba565005
Shaun Stummer <shaun.stummer1@tafensw.edu.au>
2024-10-23 11:59:35 +1100

    Updated for class ex after lunch

---
+++ FunctionUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+//USING
+
+namespace FunctionUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //HOW MANY TEST METHODS DO WE NEED
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
